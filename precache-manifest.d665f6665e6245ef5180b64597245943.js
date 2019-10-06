ad34cf7d5e42f0db28d444ca373d830c550de345
Aarish <aarish.angel@gmail.com>
2019-10-06 22:58:31 +0500

    Updates

---
+++ precache-manifest.d665f6665e6245ef5180b64597245943.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ca324aa01004426255ebc82801f89b",
+    "url": "/github-searcher/index.html"
+  },
+  {
+    "revision": "56bc09b266c5b54d50c3",
+    "url": "/github-searcher/static/css/main.f7b96f37.chunk.css"
+  },
+  {
+    "revision": "7306a859a3c0f2f63c1a",
+    "url": "/github-searcher/static/js/2.f2f21c77.chunk.js"
+  },
+  {
+    "revision": "56bc09b266c5b54d50c3",
+    "url": "/github-searcher/static/js/main.1582bdc2.chunk.js"
+  },
+  {
+    "revision": "a12a3b5be1cc6e03f1ee",
+    "url": "/github-searcher/static/js/runtime-main.886b5f76.js"
+  }
+]);
