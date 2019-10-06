7f7aa9274b34865a9db7da6868424402076409ae
Aarish <aarish.angel@gmail.com>
2019-10-06 21:48:41 +0500

    Github Finder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
@@ -11,14 +12,16 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   }
 
 
 
   searchUserHandler = async text => {
-    console.log(text);
+
     this.setState({ loading: true })
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
     this.setState({ users: res.data.items, loading: false })
@@ -34,8 +37,21 @@ class App extends Component {
     setTimeout(() => this.setState({ alert: null }), 3000)
   }
 
+  getUser = async username => {
+    this.setState({ loading: true })
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState({ user: res.data, loading: false })
+  }
+
+
+  getUserRepos = async username => {
+    this.setState({ loading: true })
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState({ repos: res.data, loading: false })
+  }
+
   render() {
-    const { loading, users } = this.state;
+    const { loading, user, repos, users } = this.state;
     return (
       <Router>
 
@@ -50,10 +66,13 @@ class App extends Component {
                     showClear={this.state.users.length > 0 ? true : false}
                     setAlert={this.setAlert}
                   />
-                  <User loading={loading} users={users} />
+                  <Users loading={loading} users={users} />
                 </Fragment>
               )} />
               <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User {...props} getUser={this.getUser} user={user} loading={loading} getUserRepos={this.getUserRepos} repos={repos} />
+              )} />
             </Switch>
           </div>
         </div>
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,34 +1,96 @@
-import React from 'react'
-import UserItem from './UserItem';
+import React, { Component, Fragment } from 'react'
 import Spinner from '../layout/spinner';
 import PropTypes from 'prop-types'
+import Repos from '../Repos';
+import { Link } from 'react-router-dom';
 
+export class User extends Component {
+    componentDidMount() {
+        this.props.getUser(this.props.match.params.login)
+        this.props.getUserRepos(this.props.match.params.login)
+    }
 
-const User = ({ loading, users }) => {
-    if (loading) {
-        return <Spinner />
-    } else {
+    static propTypes = {
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired,
+    }
+    render() {
+        const {
+            name,
+            avatar_url,
+            company,
+            location,
+            bio,
+            blog,
+            login,
+            html_url,
+            followers,
+            following,
+            public_repos,
+            public_gists,
+            hireable
+        } = this.props.user
+        const { loading, repos } = this.props;
+        if (loading) return <Spinner />
         return (
-            <div style={userStyle} >
-                {users.map(user => (
-                    <UserItem key={user.id} user={user} />
-                ))}
-            </div>
+            <Fragment>
+                <Link to='/' className='btn btn-light'>
+                    Back to Search
+                </Link>
+                hireable: {' '}
+                {hireable ? <i className="fas fa-check text-success" /> : <i className="fas fa-times-circle text-danger" />}
+                <div className="card grid-2">
+                    <div className="all-center">
+                        <img src={avatar_url} className='round-img' alt='' style={{ width: '150px' }} /> <h1>{name}</h1> <p>Location: {location}</p>
+                    </div>
+                    <div>
+                        {bio && <Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                            <a href={html_url} className='btn btn-dark my-1'>
+                                Visit Github Profile
+                    </a>
+                            <ul>
+                                <li>
+                                    {login && (
+                                        <Fragment>
+                                            <strong>Username: </strong> {login}
+                                        </Fragment>
+                                    )}
+                                </li>
+                                <li>
+                                    {blog && (
+                                        <Fragment>
+                                            <strong>Blog: </strong> {blog}
+                                        </Fragment>
+                                    )}
+                                </li>
+
+                                <li>
+                                    {company && (
+                                        <Fragment>
+                                            <strong>Company: </strong> {company}
+                                        </Fragment>
+                                    )}
+                                </li>
+                            </ul>
+                        </Fragment>
+                        }
+                        <div className="card text-center">
+                            <div className="badge badge-primary">Followers: {followers}</div>
+                            <div className="badge badge-primary">Following: {following}</div>
+                            <div className="badge badge-primary">Public Repos: {public_repos}</div>
+                            <div className="badge badge-primary">Public gists: {public_gists}</div>
+                        </div>
+                    </div>
+                </div>
+                <Repos repos={repos} />
+            </Fragment>
+
         )
     }
-}
-
-
-
-const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3 , 1fr)',
-    gridGap: '1rem'
-}
-
-User.propTypes = {
-    users: PropTypes.array.isRequired,
-    loading: PropTypes.bool.isRequired
 }
 
 export default User
